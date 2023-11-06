7f56fbf087cf8176a157251133ca1a508b3d4ace
macbook <harisgorcevic1000@gmail.com>
2023-11-06 14:28:50 +0100

    Rad

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,83 +1,43 @@
 import * as React from "react";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import "./homepage.css";
-export default function StandardImageList() {
+
+export default function HomePage() {
+  const components = [
+    <div className="containers">
+      <Card>
+        <CardMedia
+          sx={{ height: 330 }}
+          component="img"
+          alt="green iguana"
+          height="140"
+          image="myself.jpg"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Haris
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            People are good
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>,
+  ];
   return (
     <div className="containers">
-      {" "}
-      <ImageList
-        sx={{
-          width: "100%",
-          height: "100%",
-          backgroundColor: "white",
-          padding: "10px",
-        }}
-        cols={3}
-        rowHeight={600}
-        gap={10}
-      >
-        {itemData.map((item) => (
-          <ImageListItem sx={{ backgroundColor: "red" }} key={item.img}>
-            <img
-              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-              alt={item.title}
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
+      {components.map((el) => (
+        <div>{el}</div>
+      ))}
     </div>
   );
 }
-
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-  },
-];
